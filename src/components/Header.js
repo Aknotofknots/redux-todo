ebc75473f7225bea2b4bd371a009101107f3946d
Christian <christian.ahlsen@prorenata.se>
2017-10-02 20:43:03 +0200

    added list and header components

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Header = () => <h1 className="lead">Your todos of the day</h1>;
+
+export default Header;
---
+++ src/components/List.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const List = props => <ul style={{ listStyle: "none" }}>{props.children}</ul>;
+
+export default List;
